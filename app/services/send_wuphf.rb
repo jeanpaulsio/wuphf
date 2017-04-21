b1a17582315e122b764c86c41f665a0b9aa85909
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-04-21 12:34:13 -0700

    return early if no twitter connected

--- app/services/send_wuphf.rb
+++ app/services/send_wuphf.rb
@@ -29,6 +29,8 @@ class SendWuphf
   end
 
   def send_tweet
+    return if no_twitter_connected || no_recipient_twitter
+
     token  = @current_user.twitter_token_digest
     secret = @current_user.twitter_secret_digest
 
@@ -36,5 +38,13 @@ class SendWuphf
     twitter.message        = @wuphf[:message]
     twitter.twitter_handle = @wuphf[:twitter_handle]
     twitter.tweet
+  end
+
+  def no_twitter_connected
+    @current_user.twitter_token_digest.empty? || @current_user.twitter_secret_digest.empty? ? true : false
+  end
+
+  def no_recipient_twitter
+    @wuphf[:twitter_handle].empty?
   end
 end
