84f883020a59ef54fe76994cbe74ae1ae0ed448e
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-04-20 22:18:11 -0700

    recipient crud finished

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,6 +1,25 @@
 class RecipientsController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @recipient = current_user.recipients.build
+  end
+
+  def edit
+    @recipient = Recipient.find(params[:id])
+  end
+
+  def update
+    @recipient = Recipient.find(params[:id])
+    if @recipient.update_attributes(recipient_params)
+      flash[:success] = "WUPHF! Changes Saved!"
+      redirect_to dashboard_path
+    else
+      flash[:notice] = "Congrats, ya Wuphf'd yourself."
+      redirect_to dashboard_path
+    end
+  end
+
   def create
     @recipient = current_user.recipients.build(recipient_params)
 
@@ -8,7 +27,7 @@ class RecipientsController < ApplicationController
       flash[:success] = "WUPHF! #{@recipient.name} added to your Dog Pack!"
       redirect_to dashboard_path
     else
-      flash[:danger] = "Congrats, ya Wuphf'd yourself."
+      flash[:notice] = "Congrats, ya Wuphf'd yourself."
       redirect_to dashboard_path
     end
   end
