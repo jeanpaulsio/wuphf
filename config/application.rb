f775d3624afe133cd524a66905e1c4a359812806
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-04-27 19:12:09 -0700

    updates application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Wuphf
     # -- all .rb files in that directory are automatically loaded.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000'
+        origins '*'
         resource '*',
           :headers => :any,
           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
