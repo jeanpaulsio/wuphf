89b129337491358898c6a11a963549c029076684
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-05-13 17:48:43 -0700

    updates rack cors

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,9 @@ module Wuphf
     # -- all .rb files in that directory are automatically loaded.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000'
+        origins 'exp://ta-iq4.jeanpaulsio.wuphf-mobile.exp.direct:80',
+                'exp://exp.host/@jeanpaulsio/wuphf-mobile',
+                'localhost:3000'
         resource '*',
           :headers => :any,
           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
